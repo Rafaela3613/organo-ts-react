39de025bd356489d3af61b5f9664e05f938acf7e
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-03-15 15:32:35 -0300

    Migrar o Banner

--- src/componentes/Banner/index.tsx
+++ src/componentes/Banner/index.tsx
@@ -7,14 +7,14 @@ interface BannerProps {
     textoAlternativo?: string
 }
 
-export const Banner = ({ enderecoImagem, textoAlternativo } :BannerProps) => {
+const Banner = ({ enderecoImagem, textoAlternativo } :BannerProps) => {
     // JSX
     return (
         <header className="banner">
-            {/* <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/> */}
+            {/* <img src="" alt="O banner principal da página do Organo"/> */}
             <img src={enderecoImagem} alt={textoAlternativo}/>
         </header>
     )
 }
 
-export default Banner;
+export default Banner
